c64b1c9f42983568d9778cad0d253edf22fed519
ataga <37783552+ataga@users.noreply.github.com>
2022-03-07 18:01:48 +0100

    Frog model updated, to be checked on the lesson

--- AnimalMigrations/Animal/models.py
+++ AnimalMigrations/Animal/models.py
@@ -15,8 +15,7 @@ class Animal(models.Model):
         'DD': 'Data deficient',
         'NE': 'Not evaluated',
     }
-    latin_name = models.CharField(max_length=100, unique=True, choices='')
-    species_name = models.CharField(max_length=50, choices="")
+    species_name = models.CharField(max_length=50,)
     taxonomy_domain = models.CharField(max_length=50, choices="")
     taxonomy_kingdom = models.CharField(max_length=50, choices="")
     taxonomy_phylum = models.CharField(max_length=50, choices="")
@@ -31,30 +30,19 @@ class Animal(models.Model):
 
 
 class Amphibia(Animal):
-    TAX_DOMAIN_OPTIONS = ['Eucariota']
-    TAX_KINGDOM_OPTIONS = ['Animalia']
-    TAX_PHYLUM_OPTIONS = ['Chordata']
-    TAX_CLASS_OPTIONS = ['Amphibia']
-    TAX_ORDER_OPTIONS = ['Anura']
-    TAX_FAMILY_OPTIONS = ['Ranidae']
-    TAX_GENUS_OPTIONS = ['Rana']
-
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
-        self.taxonomy_domain = models.CharField(max_length=50, choices=Amphibia.TAX_DOMAIN_OPTIONS)
-        self.taxonomy_kingdom = models.CharField(max_length=50, choices=Amphibia.TAX_KINGDOM_OPTIONS)
-        self.taxonomy_phylum = models.CharField(max_length=50, choices=Amphibia.TAX_PHYLUM_OPTIONS)
-        self.taxonomy_class = models.CharField(max_length=50, choices=Amphibia.TAX_CLASS_OPTIONS)
-        self.taxonomy_order = models.CharField(max_length=50, choices=Amphibia.TAX_ORDER_OPTIONS)
-        self.taxonomy_family = models.CharField(max_length=50, choices=Amphibia.TAX_FAMILY_OPTIONS)
-        self.taxonomy_genus = models.CharField(max_length=50, choices=Amphibia.TAX_GENUS_OPTIONS)
+        self.taxonomy_domain = 'Eucariota'
+        self.taxonomy_kingdom = 'Animalia'
+        self.taxonomy_phylum = 'Chordata'
+        self.taxonomy_class = 'Amphibia'
 
 
-class Frogs(Amphibia):
+class Frog(Amphibia):
     TAX_SPECIES_OPTIONS = {
         'Kumak nizinny': 'Bombina bombina',
         'Kumak górski': 'Bombina variegata',
-        'Grzebiuszka ziemna': 'Pelobates     fuscus',
+        'Grzebiuszka ziemna': 'Pelobates fuscus',
         'Ropucha szara': 'Bufo bufo',
         'Ropucha paskówka': 'Epidalea calamita, syn. Bufo calamita',
         'Ropucha zielona': 'Bufotes viridis, syn. Pseudepidalea viridis, Bufo viridis',
@@ -65,10 +53,13 @@ class Frogs(Amphibia):
         'Żaba śmieszka': 'Pelophylax ridibundus syn.Rana ridibunda',
         'Żaba moczarowa': 'Rana arvalis',
         'Żaba zwinka': 'Rana dalmatina',
-        'Żaba trawna': 'Rana temporaria ',
+        'Żaba trawna': 'Rana temporaria',
     }
 
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
-        self.latin_name = models.CharField(max_length=100, unique=True, choices=Frogs.TAX_SPECIES_OPTIONS.keys)
-        self.species_name = models.CharField(max_length=50, choices=Frogs.TAX_SPECIES_OPTIONS.values)
+        self.taxonomy_order = 'Anura'
+        self.taxonomy_family = 'Ranidae'
+        self.taxonomy_genus = 'Rana'
+
+        self.species_name = models.CharField(max_length=50, choices=Frog.TAX_SPECIES_OPTIONS)
